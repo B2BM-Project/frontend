910e501b363fc1a8f6c59fbc2a8f5feb498cdd88
KTP00 <ktpfw1110@gmail.com>
2024-09-10 03:12:50 +0700

    first commit

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  )
+}
+
+export default NavBar
