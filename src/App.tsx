8f6989b8b38af6080d381d9a64f8d11d3985a463
KTP00 <ktpfw1110@gmail.com>
2024-09-10 17:22:03 +0700

    style: re scaling nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-import React from 'react'
 import NavBar from './components/NavBar.tsx'
-import './App.css'
 
 function App() {
   return (
-    <div className='mainContainer'>
+    <>
         <NavBar/>
-        <div><h1>home</h1></div>
+    </>
 
-
-    </div>
     
   )
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,31 +1,21 @@
-import React from "react";
 import "./NavBar.css";
-import logo from '../assets/logo4.png'
+import '../App.css'
+import logo from '../assets/logo.png'
 
 function NavBar() {
   return (
-    <nav className="nav">
+    <nav>
       <div className="nav-left">
           <img src={logo} alt="logo" />
       </div>
       <div className="nav-center">
-        <ul className="nav-ul">
-          <li>
-            <a className="navtext" href="/Learn">Learn</a>
-          </li>
-          <li>
-            <a className="navtext" href="/Challenge">Challenge</a>
-          </li>
-          <li>
-            <a className="navtext" href="/Competiton">Competiton</a>
-          </li>
-          <li>
-            <a className="navtext" href="/Package">Package</a>
-          </li>
-        </ul>
+          <a href="#">Learn</a>
+          <a href="#">Challenge</a>
+          <a href="#">Competition</a>
+          <a href="#">Package</a>
       </div>
       <div className="nav-right">
-        <button className="navButton">Sign in</button>
+          <button className="navButton">Sign in</button>
       </div>
     </nav>
   );
